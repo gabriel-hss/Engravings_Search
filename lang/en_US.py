de0eed1de8b323b9dfa4b14aeb78b4b3a7870806
Gabriel Henrique <130413761+gabriel-hss@users.noreply.github.com>
2023-07-17 19:23:35 -0300

    feat: add en_US language
    Co-authored-by: Mateus Marques <m-marqx@users.noreply.github.com>

---
+++ lang/en_US.py
@@ -0,0 +1,12 @@
+en_US = {
+    "MODIFY_ACESSORIES": "Modify Acessories",
+    "MODIFY_CLASS": "Modify Class",
+    "MODIFY_BUILD": "Modify Build",
+    "ACESSORIES": "Acessories",
+    "ACESSORIES_TYPE": "Acessories Type",
+    "ACESSORIES_STATS": "Acessories Stats",
+    "CLASS": "Class",
+    "BUILD": "Build",
+    "ENGRAVING1" :"Engraving",
+    "SEARCH": "Search",
+}
