c8b851cf2a455430a48af4b7954c088e22913960
Gabriel Henrique <130413761+gabriel-hss@users.noreply.github.com>
2023-07-17 20:05:36 -0300

    feat: add MenuCollapse class
    Co-authored-by: Mateus Marques <m-marqx@users.noreply.github.com>

--- menus.py
+++ menus.py
@@ -34,3 +34,40 @@ class dropdownMenu:
                 ),
             ]
         )
+
+class MenuCollapse:
+    def __init__(self, lang, label, component, id_prefix):
+        self.label_name = lang[label]
+        self.component = component
+        self.id_prefix = id_prefix
+
+    @property
+    def components(self):
+        button = dbc.Button(
+            [
+                self.label_name,
+                html.I(
+                    className="fa fa-chevron-down ml-2",
+                    id=f"{self.id_prefix}_icon",
+                    style={"transformY": "2px"},
+                ),
+            ],
+            id=f"{self.id_prefix}_button",
+            className="d-grid gap-2 col-6 mx-auto w-100",
+            outline=True,
+            color="secondary",
+        )
+
+        collapse = dbc.Collapse(
+            dbc.Card(
+                dbc.CardBody(
+                    self.component,
+                )
+            ),
+            id=f"{self.id_prefix}_collapse",
+            is_open=True,
+        )
+        component = dbc.Col([button, collapse])
+
+
+        return component
