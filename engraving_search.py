43df20b42ce849572a6885ddcdc93927f07b50da
Gabriel Henrique <130413761+gabriel-hss@users.noreply.github.com>
2023-07-17 20:15:06 -0300

    feat: add search methods
    Co-authored-by: Mateus Marques <m-marqx@users.noreply.github.com>

--- engraving_search.py
+++ engraving_search.py
@@ -27,3 +27,45 @@ class EngravingSearch:
             )
 
         return self.dataframe[status_cond]
+
+    def engraving_search(self, engraving_list: list[str]):
+        engraving_cond = reduce(
+            lambda x, y: x & y,
+            [
+                self.dataframe["Mandatory"].str.contains(val)
+                | self.dataframe["Secondary"].str.contains(val)
+                for val in engraving_list
+            ],
+        )
+
+        return self.dataframe[engraving_cond]
+
+    def class_search(self, class_selected: str):
+        return self.dataframe[self.dataframe["Class"] == class_selected]
+
+    def build_search(self, build_selected: str):
+        return self.dataframe[self.dataframe["Build"] == build_selected]
+
+    def aggregate_search(
+        self,
+        status_list: list[str] = None,
+        accessory: str = None,
+        engraving_list: list[str] = None,
+        class_selected: str = None,
+        build_selected: str = None,
+    ):
+        result = self.dataframe.copy()
+
+        if status_list and accessory:
+            result = self.accessory_search(status_list, accessory)
+
+        if engraving_list:
+            result = self.engraving_search(engraving_list)
+
+        if class_selected:
+            result = self.class_search(class_selected)
+
+        if build_selected:
+            result = self.build_search(build_selected)
+
+        return result
